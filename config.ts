b1b06110aa807bd1820591605af26603ecf5465c
wuyanyuwuyan <65934771+wuyanyuwuyan@users.noreply.github.com>
2022-08-23 23:35:44 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,12 +1,12 @@
 export default {
-  title: "yun yu yuan", // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: "yunyuyuan", // github账户名
+  title: "wuyan", // 网站标题
+  nickName: "无言", // 昵称
+  githubName: "wuyanyuwuyan", // github账户名
   githubRepo: "nuxt3-blog", // 勿修改
   githubBranch: "master", // 勿修改
   domain: "https://blog.halberd.cn", // rss域名
-  SEO_description: "云与原的博客，书写值得书写的东西",
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客",
+  SEO_description: "无言的博客，书写值得书写的东西",
+  SEO_keywords: "无言,无言's blog,无言,无言的博客",
 
   themeColor: "#2aa0bb", // 主题色
   CommentRepoId: "R_kgDOGRGGfg", // https://giscus.app/zh-CN
